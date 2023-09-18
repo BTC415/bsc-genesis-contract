bff1e67928d5fe304442ecee01ba9b1153354f01
Topcoder415 <alenschmitt415@gmail.com>
2023-09-18 12:09:04 +0800

    fix: improve Claimed index

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -15,33 +15,33 @@ contract MerkleDistributor is IMerkleDistributor, System {
     bytes32 public constant override merkleRoot = 0xad4aa415f872123b71db5d447df6bb417fa72c6a41737a82fdb5665e3edaa7c3; // TODO: replace with the real merkle root
 
     // This is a packed array of booleans.
-    mapping(bytes32 => bytes32) private claimedBitMap; 
-
+    mapping(uint256 => uint256) private claimedBitMap;
 
     struct ClaimInfo {
         bytes32 node;
-        bytes32 index;
+        uint256 index;
         address contractAddr;
     }
 
-    function isClaimed(bytes32 index) public view override returns (bool) {
-        bytes32 claimedWord = claimedBitMap[index];
-        uint256 indexUint = uint256(index);
-        bytes32 mask = bytes32(1 << indexUint);
+    function isClaimed(uint256 index) public view override returns (bool) {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        uint256 claimedWord = claimedBitMap[claimedWordIndex];
+        uint256 mask = (1 << claimedBitIndex);
         return claimedWord & mask == mask;
     }
-    
-    function _setClaimed(bytes32 index) private {
-        uint256 indexUint = uint256(index);
-        bytes32 mask = bytes32(1 << indexUint);
-        claimedBitMap[index] = claimedBitMap[index] | mask;
+
+    function _setClaimed(uint256 index) private {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        claimedBitMap[claimedWordIndex] = claimedBitMap[claimedWordIndex] | (1 << claimedBitIndex);
     }
 
 
     function claim(bytes32 tokenSymbol, uint256 amount, bytes calldata prefixNode, bytes calldata suffixNode, bytes calldata ownerSignature, bytes calldata approvalSignature, bytes32[] calldata merkleProof) external override {
         ClaimInfo memory claimInfo;
         claimInfo.node = keccak256(abi.encodePacked(prefixNode, tokenSymbol, amount, suffixNode));
-        claimInfo.index = keccak256(abi.encodePacked(tokenSymbol, claimInfo.node));
+        claimInfo.index = uint256(keccak256(abi.encodePacked(tokenSymbol, claimInfo.node)));
     
         // Check if the token is claimed.
         require(isClaimed(claimInfo.index), "AlreadyClaimed");
@@ -64,7 +64,7 @@ contract MerkleDistributor is IMerkleDistributor, System {
         // Unlock the token from TokenHub.
         ITokenHub(TOKEN_HUB_ADDR).unlock(claimInfo.contractAddr, msg.sender, amount);
 
-        emit Claimed(claimInfo.index, msg.sender, amount);
+        emit Claimed(tokenSymbol, msg.sender, amount);
     }
 
     function registerToken(bytes32 tokenSymbol, address contractAddr, uint256 decimals, uint256 amount, bytes calldata ownerSignature, bytes calldata approvalSignature) external override {
